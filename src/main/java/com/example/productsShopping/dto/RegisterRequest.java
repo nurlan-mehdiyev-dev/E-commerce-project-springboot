2c4d667838f4eb1af49326185e7bcac33f471667
nurlan-mehdiyev-dev <nurlan.mehdiyev.dev@gmail.com>
2025-04-28 06:00:19 +0400

    fix all project

--- src/main/java/com/example/productsShopping/dto/RegisterRequest.java
+++ src/main/java/com/example/productsShopping/dto/RegisterRequest.java
@@ -1,6 +1,5 @@
 package com.example.productsShopping.dto;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
